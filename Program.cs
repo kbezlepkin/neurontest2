d61a19d512afa9a95aaaa0784eb78373f539f299
kbezlepkin <kbezlepkin@outlook.com>
2021-09-22 14:08:25 +0300

    Настроил обучение нейронки на несколько выодных параметров. 1 скрытый слой содержит несколько нейронов, на входе 2 параметра (0нейрона), на выходе 2 параметра (без дополнительной обработки)

--- Program.cs
+++ Program.cs
@@ -5,69 +5,83 @@ namespace neurontest2
     class Program
     {
         public class Neuron //класс нейрона
-        { public float resultexit; //выход нейрона
-            private float weight1 = 0.5f,//вес на входах нейрона
-                            weight2 = 0.5f;
-            public float activefunction(int input1, int input2) //функция активации нейрона
+        { 
+            public float weight11 = 0.5f, weight2 = 0.5f,//вес на входах нейрона
+                            weight3 =  0.5f, weight4 = 0.5f;
+
+            public void Activefunction(int input1, int input2, out float resultexit1, out float resultexit2) //функция активации нейрона
             {
                 /*пример функции активации нейрона: перевод скорости мотоцикла в м/с*/
-                resultexit = (weight1 * input1) / (input2 * weight2);
-                return resultexit;
+                resultexit1 = (weight11 * input1) / (input2 * weight2);
+                resultexit2 = (weight3 * input1) / (weight4 * input2);
             }
 
 
-            public bool cikleobucheniya(int input1, int input2, float gr, bool ef) //функция обучения нейронки
+            public bool Cikleobucheniya(int input1, int input2, float gr1, float gr2, bool ef) //функция обучения нейронки
             {
-                float delta, //погрешность ответа на выходе
-                     curresult; //текущий результат
+                float delta1, delta2; //погрешность ответа на выходе
 
                 for (int iteraciya = 0; iteraciya < 10000 /*количество итераций*/; iteraciya++) //метод дял ограниченного количества итераций
                 {
-                    curresult = activefunction(input1, input2);
-                    delta = gr - curresult; //погрешность измерений
+                    Activefunction(input1, input2, out float curresult1, out float curresult2);//где curresult1, curresult2 текущий результат
+                    delta1 = gr1 - curresult1; //погрешность измерений
+                    delta2 = gr2 - curresult2;
                     //условие изменения весов
-                    if (delta > 0.005f || delta < -0.005f) 
+                    if (delta1 > 0.005f || delta1 < -0.005f) 
                     {
-                        proverkavesov(delta, curresult);
+                        Proverkavesov(delta1, curresult1, weight11, weight2, out  weight11, out weight2);
+                    }
+                    if (delta2 > 0.005f || delta2 < -0.005f)
+                    {
+                        Proverkavesov(delta2, curresult2, weight3, weight4, out weight3, out weight4);
                     }
                     //если обучение завершено
-                    else
+                    if (delta1 < 0.005f && delta1 > -0.005f && delta2 < 0.005f && delta2 > -0.005f)
                     {
                         Console.WriteLine("Обучение завершено");
                         return ef;
                     }
                     //вывод текущего состояния обучения
                     /*if (iteraciya % 200 == 0) */
-                    Console.WriteLine("Количество итераций: " + iteraciya + ". Текущая погрешность: " + delta);
+                    Console.WriteLine("Количество итераций: " + iteraciya + ". Текущая погрешности: " + delta1 + " и " + delta2);
                 }
                 Console.WriteLine("Закончилось количество попыток, обучений не завершено"); //если неуспел обучиться
                 Console.Read();
-                return ef = true;
+                ef = true;
+                return ef;
             }
 
-            void proverkavesov(float oshibka, float cr) //функция проверки весов для нейронов
+            public void Proverkavesov(float oshibka, float cr, float weighti1, float weighti2, out float w1, out float w2) //функция проверки весов для нейронов
             {
                 if (oshibka > 0)
-                { weight1 += oshibka / cr; }
+                { 
+                    w1 = weighti1 + oshibka / cr;
+                    w2 = weighti2;
+                }
                 else
-                { weight2 += oshibka / cr; }
+                { 
+                    w2 = weighti2 + oshibka / cr;
+                    w1 = weighti1;
+                }
             }
 
         }
-        public static void Main(string[] args)
+        public static void Main()
         {
             bool exitflag = false;
             int x1, x2;
             Console.WriteLine("Инициализация процесса обучения... \n" +
-                "Введите целевой результат обучения: ");
-            int goalresult = Convert.ToInt32(Console.ReadLine()); //целевой результат
-            Console.WriteLine("Введите обучающий параметр 1: ");
+                "Введите целевой результат обучения 1: ");
+            float goalresult1 = Convert.ToInt32(Console.ReadLine()); //целевой результат1
+            Console.WriteLine("Введите целевой результат обучения 2: ");
+            float goalresult2 = Convert.ToInt32(Console.ReadLine()); //целевой результат2
+            Console.WriteLine("Введите обучающий параметр 1 (расстояние): ");
             x1 = Convert.ToInt32(Console.ReadLine()); //переменная 1
-            Console.WriteLine("Введите обучающий параметр 2: ");
+            Console.WriteLine("Введите обучающий параметр 2(время): ");
             x2 = Convert.ToInt32(Console.ReadLine());//переменная 2
 
             Neuron neuron = new Neuron(); //инициализация нейрона
-            neuron.cikleobucheniya(x1,x2,goalresult, exitflag); //процесс обучения
+            neuron.Cikleobucheniya(x1,x2,goalresult1, goalresult2, exitflag); //процесс обучения
 
             if (exitflag == false)
             {
@@ -76,7 +90,8 @@ namespace neurontest2
                 x1 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Введите параметр 2: ");
                 x2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(neuron.activefunction(x1, x2));//вывод ответа
+                neuron.Activefunction(x1, x2, out var r1, out var r2); //вызов нейронки
+                Console.WriteLine("Результаты: " + r1 +" и "+ r2);//вывод ответа
                 Console.Read();
                 checkmetka: //метка повтора ввода при ошибке
                 Console.WriteLine("Повторить?  y/n");
